080cec272f253e5bcda322615180580671495b7b
fulfom <la.fulfom@gmail.com>
2020-04-01 07:01:47 +0900

    add link card2

--- assets/functions/getOGP.js
+++ assets/functions/getOGP.js
@@ -1,62 +1,23 @@
-const axios = require('axios');
-
-exports.handler = async(event, context, callback) => {
+exports.handler = (event, context, callback) => {
 
   if ('url' in event.queryStringParameters === false) {
     console.error("parameter 'url' is necessary!!");
     return;
   }
 
-  const url = event.queryStringParameters.url;
+  const targetUrl = event.queryStringParameters.url;
 
-  return axios.get(url)
-    .then(res => {
-      const el = new DOMParser().parseFromString(res.text, "text/html")
-      const headEls = (el.head.children)
-      let data = new Map();
-      let ogp = new Map();
-      let seo = new Map();
-      data.set('title',el.title);
-      Array.from(headEls).map(v => {
-          const prop = v.getAttribute('property');
-          const name = v.getAttribute('name');
-          if (prop){
-              ogp.set(prop, v.getAttribute("content"));
-          }
-          else if(name){
-              seo.set(name, v.getAttribute("content"));
-          }
-      })
-      data.set('ogp',ogp);
-      data.set('seo',seo);
-      console.log(data);
-      
-      if (!data.hasOwnProperty('title')) {
-          console.error("Error getting ogp data: no ogpData returned");
-          return res.json({ error: "no ogpData returned" });
-      } 
-      let ogpData = {};
-      ogpData['siteName'] = data.title;
-      for (let prop in data.ogp) {
-          if (/^og:/g.test(prop)) {
-              ogpData[prop.split(':')[1]] = data.ogp[prop][0];
-          }
-      }
-      for (let prop in data.seo) {
-        if (/^og:/g.test(prop)) {
-            ogpData[prop.split(':')[1]] = data.seo[prop][0];
-        }
-      }
-      console.log(JSON.stringify(ogpData));
-      callback(null, {
-        statusCode: 200,
-        "headers": { "Content-Type": "application/json; charset=utf-8"},
-        responseType: 'text',
-        body: JSON.stringify(ogpData)
-      });
-    })
-    .catch(e => ({
-      statusCode: 400,
-      body: e
-    }));
+  const metascraper = require('metascraper')([
+    require('metascraper-description')(),
+    require('metascraper-image')(),
+    require('metascraper-title')()
+  ])
+  
+  const got = require('got')
+  
+  ;(async () => {
+    const { body: html, url } = await got(targetUrl)
+    const metadata = await metascraper({ html, url })
+    console.log(metadata)
+  })()
 };
