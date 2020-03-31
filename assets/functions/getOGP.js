03ffc3fcb5fb1686559aab0559333b4104d8dd95
fulfom <la.fulfom@gmail.com>
2020-04-01 05:05:18 +0900

    add rich link

--- assets/functions/getOGP.js
+++ assets/functions/getOGP.js
@@ -1,4 +1,6 @@
-exports.handler = (event, context, callback) => {
+const axios = require('axios');
+
+exports.handler = async(event, context, callback) => {
 
   if ('url' in event.queryStringParameters === false) {
     console.error("parameter 'url' is necessary!!");
@@ -6,33 +8,55 @@ exports.handler = (event, context, callback) => {
   }
 
   const url = event.queryStringParameters.url;
-  const parser = require("ogp-parser");
-  parser(encodeURI(url), true).then(function(data) {
-    console.log(data);
-    if (!data.hasOwnProperty('title')) {
-        console.error("Error getting ogp data: no ogpData returned");
-        return res.json({ error: "no ogpData returned" });
-    } 
-    let ogpData = {};
-    ogpData['siteName'] = data.title;
-    for (let prop in data.ogp) {
-        if (/^og:/g.test(prop)) {
-            ogpData[prop.split(':')[1]] = data.ogp[prop][0];
-        }
-    }
-    for (let prop in data.seo) {
-      if (/^og:/g.test(prop)) {
-          ogpData[prop.split(':')[1]] = data.seo[prop][0];
-      }
-    }
-    console.log(JSON.stringify(ogpData));
-    callback(null, {
-      statusCode: 200,
-      "headers": { "Content-Type": "application/json; charset=utf-8"},
-      body: JSON.stringify(ogpData)
-    });
-  }).catch(function(error) {
-      console.error(error);
-  });
 
+  return axios.get(url)
+    .then(res => {
+      const el = new DOMParser().parseFromString(res.text, "text/html")
+      const headEls = (el.head.children)
+      let data = new Map();
+      let ogp = new Map();
+      let seo = new Map();
+      data.set('title',el.title);
+      Array.from(headEls).map(v => {
+          const prop = v.getAttribute('property');
+          const name = v.getAttribute('name');
+          if (prop){
+              ogp.set(prop, v.getAttribute("content"));
+          }
+          else if(name){
+              seo.set(name, v.getAttribute("content"));
+          }
+      })
+      data.set('ogp',ogp);
+      data.set('seo',seo);
+      console.log(data);
+      
+      if (!data.hasOwnProperty('title')) {
+          console.error("Error getting ogp data: no ogpData returned");
+          return res.json({ error: "no ogpData returned" });
+      } 
+      let ogpData = {};
+      ogpData['siteName'] = data.title;
+      for (let prop in data.ogp) {
+          if (/^og:/g.test(prop)) {
+              ogpData[prop.split(':')[1]] = data.ogp[prop][0];
+          }
+      }
+      for (let prop in data.seo) {
+        if (/^og:/g.test(prop)) {
+            ogpData[prop.split(':')[1]] = data.seo[prop][0];
+        }
+      }
+      console.log(JSON.stringify(ogpData));
+      callback(null, {
+        statusCode: 200,
+        "headers": { "Content-Type": "application/json; charset=utf-8"},
+        responseType: 'text',
+        body: JSON.stringify(ogpData)
+      });
+    })
+    .catch(e => ({
+      statusCode: 400,
+      body: e
+    }));
 };
