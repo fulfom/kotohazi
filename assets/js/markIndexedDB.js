acc5c0db14c7f5d53eaf055153f63f7fc027538f
fulfom <la.fulfom@gmail.com>
2020-07-22 00:31:34 +0900

    split the check-utilities

---
+++ assets/js/markIndexedDB.js
@@ -0,0 +1,48 @@
+// indexedDB
+var $likes = $('.check-heart');
+var $dones = $('.check-check');
+//
+// Define your database
+//
+var db = new Dexie("done-like");
+db.version(1).stores({
+    dones: 'id',
+    likes: 'id'
+});
+
+$likes.change(function(){
+    $this = $(this);
+    var checked = $this.prop('checked');
+    var id = $this.data('id');
+    if(checked){
+        db.likes.put({id: id});
+        $('#prob'+id).addClass('like');
+    }
+    else{
+        db.likes.delete(id);
+        $('#prob'+id).removeClass('like');
+    }
+});
+
+$dones.change(function(){
+    $this = $(this);
+    var checked = $this.prop('checked');
+    var id = $this.data('id');
+    if(checked){
+        db.dones.put({id: id});
+        $('#prob'+id).addClass('done');
+    }
+    else{
+        db.dones.delete(id);
+        $('#prob'+id).removeClass('done');
+    }
+});
+
+db.likes.toCollection().eachPrimaryKey(function(v){
+    $('#like'+v).prop('checked',true);
+    $('#prob'+v).addClass('like');
+});
+db.dones.toCollection().eachPrimaryKey(function(v){
+    $('#done'+v).prop('checked',true);
+    $('#prob'+v).addClass('done');
+});
